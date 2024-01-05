54f8fdd162c1b1e545123eb841f21c7e1e68d630
ns144 <nikolas.schaber@gmail.com>
2024-01-05 11:10:16 +0100

    added srt to s3 bucket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import boto3
 def get_tasks():
     import requests
     import json
@@ -34,19 +35,33 @@ def get_tasks():
         print("An exception occurred:", error)
         return None
 
-def get_file(filename):
-    import boto3
-
-    load_dotenv()
-    BUCKET_NAME = os.getenv('BUCKET_NAME')
+def get_s3_client():
+    #load_dotenv()
     SECRET_KEY =  os.getenv('SECRET_KEY')
     ACCESS_KEY =  os.getenv('ACCESS_KEY')
-
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+    return s3
+
+def get_file(filename):
+    #load_dotenv()
+    BUCKET_NAME = os.getenv('BUCKET_NAME')
+
+    s3 = get_s3_client()
     s3.download_file(BUCKET_NAME, filename, filename)
+
+def upload_file(filename):
+    s3 = get_s3_client()
+    BUCKET_NAME = os.getenv('BUCKET_NAME')
+    try:
+        response = s3.upload_file(filename, BUCKET_NAME, str(filename))
+    except Exception as error:
+        print("An exception occurred:", error)
+    
 
 def transcribe(tasks):
     import whisper
+    from whisper.utils import get_writer
+    from pathlib import Path
     model = whisper.load_model("base")
     files = tasks["transcripts"]
 
@@ -60,6 +75,22 @@ def transcribe(tasks):
             print(result["text"])
             os.remove(filename)
 
+            filepath = Path(filename)
+            srt_path = filepath.with_suffix('.srt')
+            writer = get_writer("srt","")
+            options = dict()
+            options["max_line_width"] = None
+            options["max_line_count"] = None
+            options["highlight_words"] = False
+
+            try:
+                writer(result, srt_path, options)
+            except:
+                print("Attempting to use an older Version of Whisper")
+                writer(result, srt_path)
+            print("DONE writing SRT: " + str(srt_path))
+            upload_file(srt_path)
+            #os.remove(srt_path)
 
 tasks = get_tasks()
 if tasks != None:
