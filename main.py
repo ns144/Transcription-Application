a2a5f322193fee8a104deffc32cd840a38ce7fdf
ns144 <nikolas.schaber@gmail.com>
2024-01-09 19:54:18 +0100

    cleanup of main.py moved api related functions to api_utils.py

--- main.py
+++ main.py
@@ -5,65 +5,7 @@ from get_secret import get_secret
 from speaker_diarization import speaker_diarization
 from utils.file_utils import write_srt, write_txt
 from utils.s3_utils import upload_file, get_file
-
-def get_tasks(secret):
-
-    # Get Api URL and Transcription Servive API Key
-    API_URL = secret["API_URL"]
-    TRANSCRIPTION_SERVICE_API_KEY = secret["TRANSCRIPTION_SERVICE_API_KEY"]
-
-    print(str(API_URL)+" Key: "+str(TRANSCRIPTION_SERVICE_API_KEY))
-
-    # Call API
-    # Set up the parameters for the API request
-    params = {'key': TRANSCRIPTION_SERVICE_API_KEY}
-    # Make the API request
-    try:
-        response = requests.get(API_URL, params=params)
-        # Check if the status code is 200 (OK)
-        if response.status_code == 200:
-            # Parse the JSON response and assign it to the 'tasks' variable
-            tasks = response.json()
-            print("API call successful. Tasks:", tasks)
-            return tasks
-        else:
-            # If the status code is not 200, print an error message
-            print(f"Error: API call failed with status code {response.status_code}")
-            return None
-    except Exception as error:
-        # handle the exception
-        print("An exception occurred:", error)
-        return None
-
-    
-def update_status(id, status, secret, preview=None):
-    MODIFY_URL = secret["MODIFY_URL"] + id
-    TRANSCRIPTION_SERVICE_API_KEY = secret["TRANSCRIPTION_SERVICE_API_KEY"]
-    params = {'key': TRANSCRIPTION_SERVICE_API_KEY}
-    if preview == None:
-        body = {'status': status}
-    else:
-        body = {'status': status, 'preview': preview}
-
-    json_body = json.dumps(body)
-
-    # Make the API request
-    try:
-        response = requests.post(MODIFY_URL, params=params, data=json_body)
-        # Check if the status code is 200 (OK)
-        if response.status_code == 200:
-            # Parse the JSON response and assign it to the 'tasks' variable
-            updated = response.json()
-            print("API call successful. Updated Task:", updated)
-            return updated
-        else:
-            # If the status code is not 200, print an error message
-            print(f"Error: API call failed with status code {response.status_code}")
-            return None
-    except Exception as error:
-        # handle the exception
-        print("An exception occurred:", error)
-        return None
+from utils.api_utils import update_status, get_tasks
 
 def transcribe(tasks):
     import whisper
