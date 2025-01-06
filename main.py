9e6a467b2b54dc6ec19999546e411208c77e046a
ns144 <nikolas.schaber@gmail.com>
2025-01-06 22:15:44 +0100

    Introduced logger.info and logger.error statements instead of relying on print statements

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ logger = logging.getLogger('transcription-application')
 logger.setLevel(logging.INFO)
 # create file handler which logs even debug messages
 fh = logging.FileHandler('/var/log/Python-Transcription-Application.log')
+# fh = logging.FileHandler('Python-Transcription-Application.log')
 fh.setLevel(logging.INFO)
 
 formatter = logging.Formatter('[%(levelname)s] %(message)s')
@@ -30,27 +31,26 @@ logger.addHandler(fh)
 
 # Redirect print statements to the logger
 
-
-class LoggerWriter:
-    def __init__(self, level):
-        self.level = level
-
-    def write(self, message):
-        if message.strip():  # Avoid logging empty strings from newlines
-            self.level(message)
-
-    def flush(self):
-        pass  # For compatibility with file-like objects
-
-
-sys.stdout = LoggerWriter(logger.info)
-sys.stderr = LoggerWriter(logger.error)
+# class LoggerWriter:
+#    def __init__(self, level):
+#        self.level = level
+#
+#    def write(self, message):
+#        if message.strip():  # Avoid logging empty strings from newlines
+#            self.level(message)
+#
+#    def flush(self):
+#        pass  # For compatibility with file-like objects
+#
+#
+# sys.stdout = LoggerWriter(logger.info)
+# sys.stderr = LoggerWriter(logger.error)
 
 try:
     instanceid = urllib.request.urlopen(
         'http://169.254.169.254/latest/meta-data/instance-id').read().decode()
 except Exception as error:
-    print(f"Could not get EC2 id: {error}")
+    logger.error(f"Could not get EC2 id: {error}")
     instanceid = 0
 
 
@@ -81,24 +81,25 @@ def transcribe(task):
     file = task
     filename = str(file["fileNameWithExt"])
     filepath = Path(filename)
+    processID = str(file["userId"])+"."+str(file["id"])
     try:
         update_status(file["id"], "PROCESSING", secret)
-        print("Transcription of:"+filename)
+        logger.info(processID + " - Transcription of:"+filename)
         # Download file from S3
         get_file(filename, secret)
         # Normalization of Audio
-        print("Convert File to Wav")
+        logger.info(processID + " - Convert File to Wav")
         subprocess.call(['ffmpeg', '-i', filename, "-ac", "1",
                         "-ar", "48000", 'audio.wav', '-y', '-loglevel', "quiet"])
         normed_audio = 'audio.wav'
         # Run speaker diarization
-        print("Speaker Diarization")
+        logger.info(processID + " - Speaker Diarization")
         speaker_segments = speaker_diarization(normed_audio, secret)
         # Speaker parts are combined where multiple segments of a speaker are not interrupted by another speaker
-        print("Condense Speakers")
+        logger.info(processID + " - Condense Speakers")
         speaker_segments = condense_speakers(speaker_segments)
         # transcription of the condensed segments
-        print("Transcribe Segments")
+        logger.info(processID + " - Transcribe Segments")
         # Transcription with FFMPEG
         # start = time.time()
         # transcribed_segments = transcribe_segments_no_print(normed_audio, speaker_segments)
@@ -122,18 +123,19 @@ def transcribe(task):
             print("Transcription with Faster Whisper Small took:",
                   elapsed, "Seconds")
         except Exception as error:
-            print("Faster Whisper failed: ", error)
+            # print("Faster Whisper failed: ", error)
             # Transcription with WhisperV3
             start = time.time()
             transcribed_segments = transcribe_segments_whisperV3(
                 normed_audio, speaker_segments)
             end = time.time()
             elapsed = end-start
-            print("Transcription with Pydup took:", elapsed, "Seconds")
-        print("Write Files")
+            logger.info(
+                f"{processID} - Transcription with Pydup / WhisperV3 Turbo took: {elapsed} Seconds")
+        logger.info(processID + " - Write Files")
         # Raw text of transcription
         text = get_text(transcribed_segments)
-        print(text[0:100])
+        # print(text[0:100])
         os.remove(filename)
         # Generation of srt compatible dict
         srt_segments = []
@@ -146,23 +148,27 @@ def transcribe(task):
         srt_path = filepath.with_suffix('.srt')
         txt_path = filepath.with_suffix('.txt')
         docx_path = filepath.with_suffix('.docx')
+        logger.info(processID + " - Write SRT File")
         write_srt(segments_as_dict, srt_path)
         upload_file(srt_path, secret)
+        logger.info(processID + " - SRT File Done")
+        logger.info(processID + " - Write TXT File")
         write_txt(text, txt_path)
         upload_file(txt_path, secret)
+        logger.info(processID + " - TXT File Done")
+        logger.info(processID + " - Write DOCX File")
         write_docx(speaker_segments=transcribed_segments, translated_segments=transcribed_segments,
                    scriptFilename=docx_path, sourcefile=file['displayFilename'], translated=False)
         upload_file(docx_path, secret)
+        logger.info(processID + " - DOCX File Done")
         os.remove(srt_path)
         os.remove(txt_path)
         os.remove(docx_path)
         update_status(file["id"], "SUCCESS", secret, text[0:500])
+        logger.info(processID + " - Transcription Done")
     except Exception as error:
-        print("Transcription failed:", error)
+        logger.error("Transcription failed:", error)
         update_status(file["id"], "FAILED", secret)
 
 
 refresh_tasks()
-# tasks = get_tasks(secret)
-# if tasks != None:
-#    transcribe(tasks)
