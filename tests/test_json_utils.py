e555bdd796245347dedf81fc27345b0746be8340
ns144 <nikolas.schaber@gmail.com>
2025-02-11 01:24:57 +0100

    Added test for JSON utils

---
+++ tests/test_json_utils.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+import sys
+import json
+
+
+class TestJSONUtils(unittest.TestCase):
+    sys.path.insert(0, os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..')))
+
+    def test_update_json(self):
+        from utils.json_utils import update_json
+
+        update_json("PROCESSING", 10, 0)
+
+        with open('progress.json') as secret_file:
+            progress = json.load(secret_file)
+        self.assertEqual(progress, {'STATUS': 'PROCESSING', 'PROG_TRANSCRIPTION': 0,
+                         'PROG_SPEAKER': 10}, "JSON not correctly saved / updated!")
+
+
+if __name__ == '__main__':
+    unittest.main()
